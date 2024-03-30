d80598e41f2323be957910a53f3f9d82a5f19bb4
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-03-30 23:54:52 +0100

    Init with old gist scripts

---
+++ src/discord-less-annoying.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         Less annoying Discord
+// @namespace    selfdocumentingcode
+// @version      0.1
+// @description  Turns off reply ping by default, adds reply ping toggle hotkey, hides gift nitro button.
+// @author       selfdocumentingcode@github
+// @match        https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+   /** Turn off reply ping */
+  let channelTextAreaEl;
+  let pingToggleEl;
+
+  const pingToggleHotkeyListener = (e) => {
+    if (e.ctrlKey && e.key === "r") {
+      e.preventDefault();
+      pingToggleEl && pingToggleEl.click();
+    }
+  };
+
+  const pingReplyObserver = new MutationObserver(() => {
+    pingToggleEl = channelTextAreaEl.querySelector("div[role=switch]");
+
+    if (pingToggleEl) {
+      if (pingToggleEl.getAttribute("aria-checked") === "true") {
+        pingToggleEl.click();
+      }
+
+      channelTextAreaEl.addEventListener("keydown", pingToggleHotkeyListener);
+    } else {
+      channelTextAreaEl.removeEventListener(
+        "keydown",
+        pingToggleHotkeyListener
+      );
+    }
+  });
+
+  setInterval(() => {
+    const channelTextAreaElLocal = document.querySelector(
+      "div[class^=channelTextArea]"
+    );
+    if (
+      channelTextAreaElLocal &&
+      channelTextAreaElLocal !== channelTextAreaEl
+    ) {
+      channelTextAreaEl = channelTextAreaElLocal;
+
+      pingReplyObserver.disconnect();
+      pingReplyObserver.observe(channelTextAreaEl, {
+        childList: true,
+        subtree: true,
+      });
+    }
+  }, 1000);
+
+   /** Hide gift button */
+   const styleElement = document.createElement("style");
+   styleElement.textContent =
+     'button[aria-label="Send a gift"] { display: none }';
+
+   document.head.appendChild(styleElement);
+})();
---
+++ src/hn-single-click-opener.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         Hacker News Single click opener
+// @namespace    selfdocumentingcode
+// @version      0.5
+// @description  Adds an [l+c] link that opens the url and the comments page in new tabs in one click. Inspired by Reddit Enhancement Suite (/r/Enhancement).
+// @author       selfdocumentingcode@github
+// @match        https://news.ycombinator.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ycombinator.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const pageHas4Tables = document.querySelectorAll("table").length === 4;
+
+  if (!pageHas4Tables) return;
+
+  const pageHasTableWithLinks =
+    document.querySelectorAll(
+      "table > tbody >tr.athing > td.title > span.titleline > a"
+    ).length > 0;
+
+  const pageHasTableWithCommentLinks =
+    document.querySelectorAll(
+      'table > tbody > tr > td.subtext > span.subline > a[href^="item"]'
+    ).length > 0;
+
+  const isRelevantPage = pageHasTableWithLinks && pageHasTableWithCommentLinks;
+
+  if (!isRelevantPage) return;
+
+  const tableWithLinks = document.querySelectorAll("table")[2];
+
+  const tableRows = tableWithLinks.querySelectorAll("tbody > tr");
+
+  for (let i = 0; i < tableRows.length; i++) {
+    const tr = tableRows[i];
+
+    // Table rows are a mix of links, comments, spacers, etc.
+    // Link rows have the class 'athing' for some reason
+    if (!tr.classList.contains("athing")) continue;
+
+    const linkUrl = tr.querySelector("td.title > span.titleline > a")?.href;
+
+    if (!linkUrl) continue;
+
+    // Row with link to comments follows link row
+    const commentsTr = tableRows[i + 1];
+
+    // This row and the next can now be skipped
+    i += 2;
+
+    const subtextContainer = commentsTr.querySelector(
+      "td.subtext > span.subline"
+    );
+
+    const isJobsLink =
+      !subtextContainer || subtextContainer.children.length === 1; // There might be a better way to test, but this fits
+
+    // Don't want to show l+c on job posts
+    if (isJobsLink) continue;
+
+    const commentsUrl = subtextContainer.querySelector("span.age > a")?.href;
+
+    subtextContainer.appendChild(document.createTextNode(" | "));
+
+    const isExternalLink = linkUrl.indexOf("news.ycombinator.com") < 0;
+
+    const lPlusCLink = document.createElement("a");
+    lPlusCLink.href = "javascript:void(0)";
+    lPlusCLink.text = isExternalLink ? "[l+c]" : "[l=c]";
+
+    lPlusCLink.onclick = () => {
+      window.open(commentsUrl, "_blank");
+      if (isExternalLink) {
+        window.open(linkUrl, "_blank");
+      }
+    };
+
+    subtextContainer.appendChild(lPlusCLink);
+  }
+})();
