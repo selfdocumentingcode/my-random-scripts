c75998600b68755e2ec3926c17043de99bb6dcca
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-08-21 22:25:54 +0200

    Use GM_openInTab instead of window.open

--- userscripts/hn-single-click-opener.user.js
+++ userscripts/hn-single-click-opener.user.js
@@ -1,48 +1,45 @@
 // ==UserScript==
 // @name         Hacker News Single click opener
 // @namespace    selfdocumentingcode
-// @version      0.5
+// @version      0.6
 // @description  Adds an [l+c] link that opens the url and the comments page in new tabs in one click. Inspired by Reddit Enhancement Suite (/r/Enhancement).
 // @author       selfdocumentingcode@github
 // @match        https://news.ycombinator.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ycombinator.com
-// @grant        none
-// @license      MIT
+// @grant        GM_openInTab
 // ==/UserScript==
 
 (function () {
-  "use strict";
+  'use strict';
 
-  const pageHas4Tables = document.querySelectorAll("table").length === 4;
+  console.log(window);
+
+  const pageHas4Tables = document.querySelectorAll('table').length === 4;
 
   if (!pageHas4Tables) return;
 
   const pageHasTableWithLinks =
-    document.querySelectorAll(
-      "table > tbody >tr.athing > td.title > span.titleline > a"
-    ).length > 0;
+    document.querySelectorAll('table > tbody >tr.athing > td.title > span.titleline > a').length > 0;
 
   const pageHasTableWithCommentLinks =
-    document.querySelectorAll(
-      'table > tbody > tr > td.subtext > span.subline > a[href^="item"]'
-    ).length > 0;
+    document.querySelectorAll('table > tbody > tr > td.subtext > span.subline > a[href^="item"]').length > 0;
 
   const isRelevantPage = pageHasTableWithLinks && pageHasTableWithCommentLinks;
 
   if (!isRelevantPage) return;
 
-  const tableWithLinks = document.querySelectorAll("table")[2];
+  const tableWithLinks = document.querySelectorAll('table')[2];
 
-  const tableRows = tableWithLinks.querySelectorAll("tbody > tr");
+  const tableRows = tableWithLinks.querySelectorAll('tbody > tr');
 
   for (let i = 0; i < tableRows.length; i++) {
     const tr = tableRows[i];
 
     // Table rows are a mix of links, comments, spacers, etc.
     // Link rows have the class 'athing' for some reason
-    if (!tr.classList.contains("athing")) continue;
+    if (!tr.classList.contains('athing')) continue;
 
-    const linkUrl = tr.querySelector("td.title > span.titleline > a")?.href;
+    const linkUrl = tr.querySelector('td.title > span.titleline > a')?.href;
 
     if (!linkUrl) continue;
 
@@ -52,31 +49,28 @@
     // This row and the next can now be skipped
     i += 2;
 
-    const subtextContainer = commentsTr.querySelector(
-      "td.subtext > span.subline"
-    );
+    const subtextContainer = commentsTr.querySelector('td.subtext > span.subline');
 
-    const isJobsLink =
-      !subtextContainer || subtextContainer.children.length === 1; // There might be a better way to test, but this fits
+    const isJobsLink = !subtextContainer || subtextContainer.children.length === 1; // There might be a better way to test, but this fits
 
     // Don't want to show l+c on job posts
     if (isJobsLink) continue;
 
-    const commentsUrl = subtextContainer.querySelector("span.age > a")?.href;
+    const commentsUrl = subtextContainer.querySelector('span.age > a')?.href;
 
-    subtextContainer.appendChild(document.createTextNode(" | "));
+    subtextContainer.appendChild(document.createTextNode(' | '));
 
-    const isExternalLink = linkUrl.indexOf("news.ycombinator.com") < 0;
+    const isExternalLink = linkUrl.indexOf('news.ycombinator.com') < 0;
 
-    const lPlusCLink = document.createElement("a");
-    lPlusCLink.href = "javascript:void(0)";
-    lPlusCLink.text = isExternalLink ? "[l+c]" : "[l=c]";
+    const lPlusCLink = document.createElement('a');
+    lPlusCLink.href = 'javascript:void(0)';
+    lPlusCLink.text = isExternalLink ? '[l+c]' : '[l=c]';
 
     lPlusCLink.onclick = () => {
-      window.open(commentsUrl, "_blank");
       if (isExternalLink) {
-        window.open(linkUrl, "_blank");
+        GM_openInTab(linkUrl);
       }
+      GM_openInTab(commentsUrl);
     };
 
     subtextContainer.appendChild(lPlusCLink);
